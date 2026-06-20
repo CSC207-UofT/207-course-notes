ae64590f948c940651083317dfc6f975fbab1bea
Guy Khazma <guykhag@gmail.com>
2026-06-20 14:49:06 -0400

    Use a bundled checkstyle

--- code/src/test/java/cs/toronto/edu/csc207/hello/HelloWorldTest.java
+++ code/src/test/java/cs/toronto/edu/csc207/hello/HelloWorldTest.java
@@ -1,12 +1,10 @@
 package cs.toronto.edu.csc207.hello;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Sample test class demonstrating the test folder structure and JUnit 5.
- */
+import org.junit.jupiter.api.Test;
+
+/** Sample test class demonstrating the test folder structure and JUnit 5. */
 class HelloWorldTest {
 
   @Test
